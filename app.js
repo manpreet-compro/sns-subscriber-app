5dbb154f5e69090f17bd3e06af92f0ca66cb30f2
shashank <shashank.jha@comprotechnologies.com>
2020-02-05 13:24:30 +0530

    some change

--- app.js
+++ app.js
@@ -6,32 +6,40 @@ const rp = require('request-promise');
 app.use(bodyParser.json({ limit: '1mb' }));
 
 app.post('/sns', (req, res) => {
-    if(req.headers['x-amz-sns-message-type'] === 'SubscriptionConfirmation') {
-        console.log("Headers - " + JSON.stringify(req.headers));
-        console.log("Body - " + JSON.stringify(req.body));
-        const subscriptionConfirmUrl = req.body.SubscribeURL;
-        console.log("url - " + subscriptionConfirmUrl);
-        const reqOptions = {
-            uri: subscriptionConfirmUrl,
-            method: 'GET'
-        };
-        rp(reqOptions)
-            .then((response) => {
-                console.log(`Subscription Confirmed. Response: ${response}`);
-                // return res.send(`Subscription Confirmed. Response: ${response}`);
+    let body;
+    req.on('data', (chunk) => {
+        body += chunk.toString()
+      })
+    
+    req.on('end', () => {
+        let payload = JSON.parse(body)
+        if(req.headers['x-amz-sns-message-type'] === 'SubscriptionConfirmation') {
+            console.log("Headers - " + JSON.stringify(req.headers));
+            console.log("Body - " + JSON.stringify(payload));
+            const subscriptionConfirmUrl = payload.SubscribeURL;
+            console.log("url - " + subscriptionConfirmUrl);
+            const reqOptions = {
+                uri: subscriptionConfirmUrl,
+                method: 'GET'
+            };
+            rp(reqOptions)
+                .then((response) => {
+                    console.log(`Subscription Confirmed. Response: ${response}`);
+                    // return res.send(`Subscription Confirmed. Response: ${response}`);
+                })
+                .catch((err) => {
+                    console.log(`Could not confirm subscription. Error: ${err}`);
+                    // return res.send(`Could not confirm subscription. Error: ${err}`);
+                });
+        }
+        else if(req.headers['x-amz-sns-message-type'] === 'notification') {
+            console.log(`subject: ${payload.subject}, message: ${payload.message}`);
+            return res.json({
+                subject: payload.subject,
+                message: payload.message
             })
-            .catch((err) => {
-                console.log(`Could not confirm subscription. Error: ${err}`);
-                // return res.send(`Could not confirm subscription. Error: ${err}`);
-            });
-    }
-    else if(req.headers['x-amz-sns-message-type'] === 'notification') {
-        console.log(`subject: ${req.body.subject}, message: ${req.body.message}`);
-        return res.json({
-            subject: req.body.subject,
-            message: req.body.message
-        })
-    }
+        }
+    });
 });
 
 app.get('/status', (req, res) => {
